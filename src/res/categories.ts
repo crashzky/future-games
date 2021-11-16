9d0db56cd41611fc8b1098972144ad231dc11c9a
nmzgnv <nick-mozg1@mail.ru>
2021-11-16 22:47:26 +0500

    Refactor: category object

---
+++ src/res/categories.ts
@@ -0,0 +1,16 @@
+import {Category} from "./Category";
+
+export const availableCategories: Category[] = [
+    {
+        value: 'Здоровье человека',
+        backgroundStyle:'bg-red-400',
+    },
+    {
+        value: 'Источники энергообеспечения города',
+        backgroundStyle: 'bg-blue-700',
+    },
+    {
+        value: 'Среда доступных возможностей',
+        backgroundStyle: 'bg-purple-500',
+    },
+];
